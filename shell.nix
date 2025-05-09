343465ecbf9e66a8c97e919ad1b046543545e128
Randall <rkliman11@gmail.com>
2025-05-09 13:39:37 -0700

    refactor: comment out unused Rust packages and clean up shell configuration

--- shell.nix
+++ shell.nix
@@ -7,26 +7,13 @@ pkgs.mkShell {
     pkgs.python311Packages.numpy  # Optional, remove if unused
     pkgs.python311Packages.matplotlib
     pkgs.python311Packages.seaborn
-    pkgs.python311Packages.pylatex
 
-    pkgs.rustc
-    pkgs.cargo
+    # pkgs.rustc
+    # pkgs.cargo
 
     pkgs.rust-analyzer
-    pkgs.openssl
-    pkgs.pkg-config
-    pkgs.graphite2
-    pkgs.freetype
-    pkgs.icu
-    pkgs.libgcc
-    pkgs.harfbuzz
-    # pkgs.libg
 
     pkgs.typst
   ];
-
-  shellHook = ''
-    export CXXFLAGS="-O2"
-  '';
 }
 
